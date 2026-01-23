c07541833cb7cf3446b79bc38c3386744971db86
Martijn Zeestraten <git@martijnzeestraten.nl>
2026-01-23 12:49:11 +0100

    Updated positioning

---
+++ cases-data.js
@@ -0,0 +1,50 @@
+window.MARTLET_CASES = [
+  {
+    slug: "madern",
+    title: "40% kortere doorlooptijd door structurele planningsverbetering",
+    tags: ["Automatisering", "Leiderschap"],
+    challenge: "Madern moest lead-time en leverbetrouwbaarheid verbeteren om competitief te blijven. De problematiek was bekend, maar vastgelopen in dagelijkse herplanning, buffers en ad-hoc ingrepen.",
+    contribution: [
+      "Structureren van complexe planningsproblematiek tot beslisbare oorzaken",
+      "Verleggen van focus van procestijden naar wachttijden en flow",
+      "Vertalen van analyse naar een haalbare technische route",
+      "Inhoudelijk realiseren van de oplossing (niet alleen regie)"
+    ],
+    resultsShort: [
+      "Meer focus in werkvoorbereiding",
+      "Productieleiding stuurt op voortgang",
+      "Realtime planoverzicht",
+      "Vroegtijdig bijsturen"
+    ],
+    results: [
+      "Werkvoorbereiders besteden hun tijd aan inhoudelijke werkvoorbereiding, niet aan continu (her)plannen",
+      "Productieleiders focussen op productie-inhoud en voortgang, in plaats van volgorde- en brandjesproblematiek",
+      "Realtime overzicht in de planning over afdelingen en orders",
+      "Vroegtijdig inzicht in potentiële levertijdproblemen, waardoor bijsturen vooraf mogelijk werd"
+    ],
+    application: "Productieomgeving (MKB)"
+  },
+  {
+    slug: "afvalafvoer-optimalisatie",
+    title: "600% hogere afvoercapaciteit zonder zwaardere pomp",
+    tags: ["Product"],
+    challenge: "De prestaties van een afvalafvoersysteem voldeden niet meer aan markteisen. De bestaande aanpak focuste op meer onderdruk en grotere pompen, zonder het gewenste effect.",
+    contribution: [
+      "Terugbrengen van het probleem tot de bepalende fysische driver",
+      "Doorbreken van aannames over pompvermogen als bottleneck",
+      "Ontwerpbeslissingen op basis van metingen en stromingsanalyse",
+      "Begeleiden van simulatie en ontwerp tot realiseerbaar eindresultaat"
+    ],
+    resultsShort: [
+      "+600% stroomsnelheid",
+      "Punch-snelheid op niveau",
+      "Significante kostprijsreductie"
+    ],
+    results: [
+      "+600% stroomsnelheid",
+      "Punch-snelheid conform klantwensen",
+      "Significante kostprijsreductie"
+    ],
+    application: "Industrieel machineontwerp"
+  }
+];
